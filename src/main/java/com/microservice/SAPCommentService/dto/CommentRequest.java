5470495cac3408bc331fffe2e64e5f8c189d7234
r9988.2588 <r9988.2588@gmail.com>
2025-09-04 05:05:38 +0000

    fixed dto's

--- src/main/java/com/microservice/SAPCommentService/dto/CommentRequest.java
+++ src/main/java/com/microservice/SAPCommentService/dto/CommentRequest.java
@@ -1,14 +1,18 @@
 package com.microservice.SAPCommentService.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CommentRequest {
-    private Long commentId;
 
+    @NotNull
     private Long postId;
 
+    @NotNull
     private Long authorId;
-    
+
+    @NotBlank
     private String content;
 }
--- src/main/java/com/microservice/SAPCommentService/dto/CommentResponse.java
+++ src/main/java/com/microservice/SAPCommentService/dto/CommentResponse.java
@@ -1,18 +1,15 @@
 package com.microservice.SAPCommentService.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class CommentResponse {
 
-    @NotNull
+    private Long commentId;
+
     private Long postId;
 
-    @NotNull
     private Long authorId;
 
-    @NotBlank
     private String content;
 }
